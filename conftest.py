9e14396c6da9e24755b11a913a1a42641a218111
Andrei Hetsiuk <Andrei.Hetsiuk@qaddress.com>
2020-07-20 16:16:53 +0300

    added conftest.py

---
+++ conftest.py
@@ -0,0 +1,16 @@
+import pytest
+from selenium import webdriver
+
+
+
+@pytest.fixture(scope="function")
+
+def browser():
+    print("\nstart browser for test..")
+    browser = webdriver.Chrome()
+    yield browser
+    print(type(browser))
+    browser.quit()
+    
+
+
