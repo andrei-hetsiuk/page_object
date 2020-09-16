dac50c611c78aa3140bc73aa8352f9b8a6297c5b
Andrei Hetsiuk <Andrei.Hetsiuk@qaddress.com>
2020-09-16 11:13:51 +0300

    the test of main page was added

---
+++ pages/base_page.py
@@ -0,0 +1,10 @@
+import self as self
+
+
+class BasePage:
+    def __init__(self, browser, url):
+        self.browser = browser
+        self.url = url
+
+    def open(self):
+        self.browser.get(self.url)
